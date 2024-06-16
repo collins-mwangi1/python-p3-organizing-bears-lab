e780bacb18de3a00e1cb82480cb7ce5e838a57fc
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-06-16 12:18:53 +0300

    commits done

--- lib/testing/insert_test.py
+++ lib/testing/insert_test.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-
 import sqlite3
 
+# Connect to an in-memory SQLite database
 connection = sqlite3.connect(":memory:")
-
 cursor = connection.cursor()
 
+# Execute create.sql to create the bears table
 create_file = open("lib/create.sql")
 create_as_string = create_file.read()
 cursor.executescript(create_as_string)
 
+# Execute insert.sql to populate the bears table
 insert_file = open("lib/insert.sql")
 insert_as_string = insert_file.read()
 cursor.executescript(insert_as_string)
@@ -20,10 +20,9 @@ class TestInsert:
     def test_inserts_eight_bears_into_table(self):
         '''inserts 8 bears into bears table.'''
         result = cursor.execute("SELECT COUNT(*) FROM bears;")
-        assert(result.fetchall()[0][0] == 8)
+        assert result.fetchall()[0][0] == 8
 
     def test_has_unnamed_bear(self):
         '''inserts one unnamed bear into bears table.'''
         result = cursor.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;")
-        assert(result.fetchall()[0][0] == 1)
-    
+        assert result.fetchall()[0][0] == 1
